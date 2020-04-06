23dc710b0ef1ca92e6012c3b9af62852c1223761
yocan <1091270264@qq.com>
2020-04-06 21:18:26 +0800

    feat:如何判断一个链表是否有环

---
+++ algorithm-practice/com/yocan/algorithm/practice/IsCircleLinked.java
@@ -0,0 +1,24 @@
+package com.yocan.algorithm.practice;
+
+/**
+ * @author liuyongkang
+ * 如何判断一个链表是否有环
+ * 思路：追赶法
+ */
+public class IsCircleLinked {
+
+
+
+    private static boolean isCircle(ReverseLinked.ListNode listNode){
+
+        return true;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
