b52885e7c75a97cb337aa8beb7e7df3b28a1355b
yocan <1091270264@qq.com>
2020-06-11 18:41:26 +0800

    feat:每日温度-739

---
+++ algorithm-practice/com/yocan/algorithm/practice/DailyTemperaturesSolution.java
@@ -0,0 +1,90 @@
+package com.yocan.algorithm.practice;
+
+/**
+ * @author liuyongkang
+ * 根据每日 气温 列表，请重新生成一个列表，
+ * 对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+ */
+public class DailyTemperaturesSolution {
+
+    /**
+     * 暴力法
+     * 效率不高
+     *  平均执行时间1503 ms
+     * @param
+     * @return
+     */
+    public int[] dailyTemperatures(int[] T) {
+        int[] result = new int[T.length];
+        for (int i = 0; i < T.length-1; i++) {
+            int j = i;
+            int temp = 1;
+            while (temp < T.length - i) {
+                if (T[i] < T[j + 1]) {
+                    result[i] = temp;
+                    break;
+                } else {
+                    temp++;
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 进阶
+     * 优化一部分
+     * 平均执行时间339 ms
+     * @param T
+     * @return
+     */
+    public int[] dailyTemperaturesApprove(int[] T) {
+        int[] result = new int[T.length];
+        for (int i = 0; i < T.length-1; i++) {
+            int j = i;
+            int temp = 1;
+            //争取多用之前的结果
+            if (i > 0) {
+                if (result[i - 1] == 0 && T[i] >= T[i - 1]) {
+                    continue;
+                }
+                if (T[i] < T[i - 1] && result[i - 1] != 0) {
+                    while (temp < result[i - 1] + i) {
+                        if (j >= result[i - 1] + i - 1) {
+                            result[i] = result[i - 1];
+                            break;
+                        }
+                        if (T[i] < T[j + 1]) {
+                            result[i] = temp;
+                            break;
+                        } else {
+                            temp++;
+                            j++;
+                        }
+                    }
+                    continue;
+                }
+                if (T[i] == T[i - 1]) {
+                    result[i] = result[i - 1] - 1;
+                    continue;
+                }
+
+            }
+            while (temp < T.length - i) {
+                if (T[i] < T[j + 1]) {
+                    result[i] = temp;
+                    break;
+                } else {
+                    temp++;
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+
+}
